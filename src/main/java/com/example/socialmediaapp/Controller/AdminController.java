4c03bbe713b5c2b99f9a1f6979d419cf1a5fe255
Param-Veer-Singh <paramveersinghchampawat@gmail.com>
2023-12-19 01:15:31 +0530

    admin service

--- src/main/java/com/example/socialmediaapp/Controller/AdminController.java
+++ src/main/java/com/example/socialmediaapp/Controller/AdminController.java
@@ -1,4 +1,27 @@
 package com.example.socialmediaapp.Controller;
 
+import com.example.socialmediaapp.Models.Admin;
+import com.example.socialmediaapp.RequestDtos.AddUserDto;
+import com.example.socialmediaapp.Service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin")
 public class AdminController {
+
+    @Autowired
+    private AdminService adminService;
+    public ResponseEntity addAdmin(@RequestBody Admin admin){
+        try{
+            return adminService.addAdmin(admin);
+        }
+        catch (Exception e){
+            return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
 }
--- src/main/java/com/example/socialmediaapp/Service/AdminService.java
+++ src/main/java/com/example/socialmediaapp/Service/AdminService.java
@@ -1,4 +1,22 @@
 package com.example.socialmediaapp.Service;
 
+import com.example.socialmediaapp.Models.Admin;
+import com.example.socialmediaapp.Repository.AdminRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AdminService {
+
+    @Autowired
+    private AdminRepository adminRepository;
+
+    public ResponseEntity addAdmin(Admin admin){
+        adminRepository.save(admin);
+
+        return new ResponseEntity("Admin saved", HttpStatus.OK);
+    }
 }
